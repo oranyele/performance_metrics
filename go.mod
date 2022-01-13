19f515bfb0bfc836fc9ab9b97adfa8aa1f2e6a20
Ranyele Amorim <ranyele.amorim@mercadolivre.com>
2022-01-12 23:05:58 -0300

    Module created

---
+++ go.mod
@@ -0,0 +1,3 @@
+module com.mercadolibre/performance/decompose
+
+go 1.17
---
+++ internal/metrics/decompose/decompose_metrics.go
@@ -0,0 +1,8 @@
+package decompose_metrics
+
+import "fmt"
+
+func Hello(name string) string {
+    message := fmt.Sprintf("Hi, %v. Welcome!", name)
+    return message
+}
